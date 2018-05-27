8b54953439d9f705ed9a406cfa0293f22d252475
Danny Hawk <HawkDanny@users.noreply.github.com>
2018-05-27 12:37:39 -0400

    Day 2 completed

--- Rocks/Assets/Scripts/Rotation.cs
+++ Rocks/Assets/Scripts/Rotation.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 public class Rotation : MonoBehaviour {
 
     public float degreesPerSecond = 1; //Default to 1
-	
-	// Update is called once per frame
+    public Material day2;
+
+    private float startingRotation;
+    private float deltaRotation;
+
+
+    void Setup() {
+        startingRotation = this.transform.eulerAngles.y;
+    }
+
 	void Update () {
         this.transform.Rotate(new Vector3(this.transform.rotation.x, this.transform.rotation.y + degreesPerSecond, this.transform.rotation.z) * Time.deltaTime);
+
+        deltaRotation = Mathf.Abs(this.transform.eulerAngles.y - startingRotation);
+
+        day2.SetFloat("Vector1_38DB6B22", Mathf.Min(deltaRotation / 360, 1.0f));
 	}
 }
