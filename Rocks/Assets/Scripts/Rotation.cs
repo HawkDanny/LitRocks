838d40d064103ebd999a28807a1b443904ee6133
Danny Hawk <HawkDanny@users.noreply.github.com>
2018-05-27 09:57:58 -0400

    Initial commit

---
+++ Rocks/Assets/Scripts/Rotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+
+    public float degreesPerSecond = 1; //Default to 1
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(new Vector3(this.transform.rotation.x, this.transform.rotation.y + degreesPerSecond, this.transform.rotation.z) * Time.deltaTime);
+	}
+}
